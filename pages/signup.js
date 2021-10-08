bf40c70ddf5946c6ac3cee7e77b1c4c4d3c121e3
Alexander Roshchin <alexander.roshchin85@gmail.com>
2021-10-08 14:13:24 +0300

    Added layout to Sign up page

--- pages/signup.js
+++ pages/signup.js
@@ -1,11 +1,140 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { FooterMessage, HeaderMessage } from '../components/Common/WelcomeMessage';
+import { Button, Divider, Form, Message, Segment } from 'semantic-ui-react';
+
+const regexUserName = /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/;
 
 const Signup = () => {
-    return (
-        <div>
-            sign up page
-        </div>
-    );
+	const [user, setUser] = useState({
+		name: '',
+		email: '',
+		password: '',
+		bio: '',
+		facebook: '',
+		youtube: '',
+		twitter: '',
+		instagram: '',
+	});
+
+	const { name, email, password, bio } = user;
+
+	const [showSocialLinks, setShowSocialLinks] = useState(false);
+	const [showPassword, setShowPassword] = useState(false);
+	const [errorMsg, setErrorMsg] = useState(null);
+	const [formLoading, setFormLoading] = useState(false);
+	const [submitDisabled, setSubmitDisabled] = useState(true);
+
+	const [username, setUsername] = useState('');
+	const [usernameLoading, setUsernameLoading] = useState(false);
+	const [usernameAvailable, setUsernameAvailable] = useState(false);
+
+	const handleChange = e => {
+		const { name, value, files } = e.target;
+
+		setUser(prev => ({ ...prev, [name]: value }));
+	};
+
+	const handleSubmit = async e => {
+		e.preventDefault();
+		setFormLoading(true);
+	};
+
+	useEffect(() => {
+		const isUser = Object.values({ name, email, password, bio }).every(item =>
+			Boolean(item)
+		);
+		isUser ? setSubmitDisabled(false) : setSubmitDisabled(true);
+	}, [user]);
+
+	return (
+		<div>
+			<HeaderMessage/>
+			<Form loading={formLoading} error={errorMsg !== null} onSubmit={handleSubmit}>
+				<Message
+					error
+					header="Oops!"
+					content={errorMsg}
+					onDismiss={() => setErrorMsg(null)}
+				/>
+
+				<Segment>
+
+					<Form.Input
+						required
+						label="Name"
+						placeholder="Name"
+						name="name"
+						value={name}
+						onChange={handleChange}
+						fluid
+						icon="user"
+						iconPosition="left"
+					/>
+
+					<Form.Input
+						required
+						label="Email"
+						placeholder="Email"
+						name="email"
+						value={email}
+						onChange={handleChange}
+						fluid
+						icon="envelope"
+						iconPosition="left"
+						type="email"
+					/>
+
+					<Form.Input
+						label="Password"
+						placeholder="Password"
+						name="password"
+						value={password}
+						onChange={handleChange}
+						fluid
+						icon={{
+							name: "eye",
+							circular: true,
+							link: true,
+							onClick: () => setShowPassword(!showPassword)
+						}}
+						iconPosition="left"
+						type={showPassword ? "text" : "password"}
+						required
+					/>
+
+					<Form.Input
+						loading={usernameLoading}
+						error={!usernameAvailable}
+						required
+						label="Username"
+						placeholder="Username"
+						value={username}
+						onChange={e => {
+							setUsername(e.target.value);
+							if (regexUserName.test(e.target.value)) {
+								setUsernameAvailable(true);
+							} else {
+								setUsernameAvailable(false);
+							}
+						}}
+						fluid
+						icon={usernameAvailable ? "check" : "close"}
+						iconPosition="left"
+					/>
+
+					<Divider hidden />
+					<Button
+						icon="signup"
+						content="Signup"
+						type="submit"
+						color="orange"
+						disabled={submitDisabled || !usernameAvailable}
+					/>
+				</Segment>
+			</Form>
+			<FooterMessage/>
+		</div>
+	);
 };
 
 export default Signup;
